bf83caa12f5197ee3e1598616bd43449ad263295
vel-3633f <vel3633fu@gmail.com>
2024-01-06 10:54:54 +0900

    tsの型エラーを解決

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,32 @@ import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import "./App.css";
 import { Image, ScrollControls, Scroll, useScroll } from "@react-three/drei";
 import { useRef } from "react";
+import { Group } from "three";
 
 function Images() {
-  const { width, height } = useThree((state) => state.viewport);
-  const group = useRef();
+  const { height } = useThree((state) => state.viewport);
+  const group = useRef<Group>(null!);
   const data = useScroll();
 
   useFrame(() => {
-    group.current.children[0].material.zoom = 1 + data.range(0, 1 / 3) / 3;
-    group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3;
-    group.current.children[2].material.zoom = 1 + data.range(0, 1 / 3) / 3;
-    group.current.children[3].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+    for (let i = 0; i < 4; i++) {
+      // @ts-ignore
+      group.current.children[i].material.zoom = 1 + data.range(0, 1 / 3) / 3;
+    }
   });
   return (
     <group ref={group}>
-      <Image url="./img1.jpg" scale={[4, height, 1]} position={[-1, 0, 1]} />
+      <Image url="./img1.jpg" scale={[4, height]} position={[-1, 0, 1]} />
       <Image url="./img2.jpg" scale={3} position={[2, 0, 1]} />
+      <Image url="./img3.jpg" scale={[1, 3.5]} position={[-2.3, -height, 2]} />
       <Image
         url="./img3.jpg"
-        scale={[1, 3.5, 1]}
-        position={[-2.3, -height, 2]}
-      />
-      <Image
-        url="./img3.jpg"
-        scale={[1, 2.7, 1]}
+        scale={[1, 2.7]}
         position={[-1.4, -height - 0.7, 1]}
       />
       <Image
         url="./img4.jpg"
-        scale={[1.4, 2, 1]}
+        scale={[1.4, 2]}
         position={[1.3, -height - 0.3, 3.2]}
       />
     </group>
